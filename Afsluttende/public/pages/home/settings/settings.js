98404e2c3653d1701af623cfd8a323ffce224aa0
Rasmus Trap <rasm510f@stud.kea.dk>
2022-05-09 14:23:23 +0200

    Settings page style osv

--- Afsluttende/public/pages/home/settings/settings.js
+++ Afsluttende/public/pages/home/settings/settings.js
@@ -1,1 +1,11 @@
-localStorage.getItem("session")
+let session = localStorage.getItem("session")
+let jsonSes = JSON.parse(session)
+
+if(jsonSes.role !== "admin"){
+    document.getElementById("currency").style.display = "none"
+    document.getElementById("change-admin-password").style.display = "none"
+    document.getElementById("change-member-password").style.display = "none"
+    document.getElementById("new-season").style.display = "none"
+    document.getElementById("export-team-info").style.display = "none"
+    document.getElementById("team-settings").style.display = "none"
+}
