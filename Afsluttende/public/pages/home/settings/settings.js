d66e39c74061e2fd3507710296af4c7cd0a8cfb6
Rasmus Trap <rasm510f@stud.kea.dk>
2022-05-24 14:58:41 +0200

    new season

--- Afsluttende/public/pages/home/settings/settings.js
+++ Afsluttende/public/pages/home/settings/settings.js
@@ -1,7 +1,10 @@
-let session = localStorage.getItem("session")
-let jsonSes = JSON.parse(session)
+if(localStorage.getItem("session") === null){
+    window.location = "/"
+}
 
-if(jsonSes.role !== "admin"){
+let session = JSON.parse(localStorage.getItem("session"))
+
+if(session.role !== "admin"){
     document.getElementById("currency").style.display = "none"
     document.getElementById("change-admin-password").style.display = "none"
     document.getElementById("change-member-password").style.display = "none"
@@ -26,4 +29,40 @@ function payment(){
 document.getElementById("monthly").onclick = function() {monthly()}
 function monthly(){
     window.location = "/monthly"
+}
+
+document.getElementById("new-season").onclick = function() {newSeason()}
+function newSeason(){
+console.log(session.accessToken)
+    cuteAlert({
+        type: 'question',
+        title: 'New season',
+        message: 'Are you sure blah blah',
+        confirmText: "I am sure!",
+        cancelText: "Do not reset"
+    }).then((e) => {
+        if(e == "confirm"){
+            fetch("http://192.168.0.107:5000/api/User/reset", {
+            method: "POST",
+            headers: { 'Authorization': `Bearer ${session.accessToken}`,
+                "Content-type": "application/json; charset=UTF-8" }   
+        }).then(res => {
+            if(res.status !== 200 ){
+                cuteToast({
+                    type: 'warning', // or 'info', 'error', 'warning',
+                    title: "Warning",
+                    message: "Something went wrong",
+                    timer: 5000
+                  })
+            }else if(res.status === 200){
+                cuteToast({
+                    type: 'success', // or 'info', 'error', 'warning',
+                    title: "Success",
+                    message: "Season reset",
+                    timer: 5000
+                  })
+            }
+        })
+    }
+    })
 }
